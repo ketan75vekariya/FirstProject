b6eb98fb5f8eb05dd07d2c15c0d3b7690a75d07c
ketan75vekariya <ketan75vekariya@yahoo.com>
2025-01-08 15:28:33 +0000

    Hello Vue 3

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello Vue 3!'
+    }
+  }
+}
+</script>
 <template>
  <h1 class="text-3xl font-bold underline">
-    Hello world!
+    {{message}}
   </h1>
 </template>
