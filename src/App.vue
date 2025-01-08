e5c30decab1c52f0abfbfd83680e52a9f0504e54
ketan75vekariya <ketan75vekariya@yahoo.com>
2025-01-08 15:45:56 +0000

    Array and v-for

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,21 @@ export default {
     return {
       message: 'Hello Vue 3!',
       status:'pending',
+      tasks:['task1','task2','task3'],
     }
   }
 }
 </script>
 <template>
- <h1 class="text-3xl font-bold underline">
-    {{message}}
-  </h1>
-  <p v-if = "status === 'success'">You are active</p>
-  <p v-else-if = "status === 'pending'">You are pending</p>
-  <p v-else>You are inactive</p>
+  <div class="container mx-auto">
+    <h1 class="text-3xl font-bold underline">
+      {{message}}
+    </h1>
+    <p v-if = "status === 'success'">You are active</p>
+    <p v-else-if = "status === 'pending'">You are pending</p>
+    <p v-else>You are inactive</p>
+    <ul class="list-disc bg-red-400">
+      <li v-for="task in tasks" :key="task">{{task}}</li>
+    </ul>
+  </div>
 </template>
