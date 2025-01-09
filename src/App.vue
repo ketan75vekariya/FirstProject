b73bb969dc7f50227baa5ce6f974215f9758e7bf
ketan75vekariya <ketan75vekariya@yahoo.com>
2025-01-09 00:09:43 +0000

    Added job listing section and integrated json data

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
 import Hero from './components/Hero.vue';
 import Navbar from './components/Navbar.vue';
+import HomeCard from './components/HomeCard.vue';
+import JobListings from './components/JobListings.vue';
 </script>
 
 <template>
   <Navbar />
   <Hero title="Test Title" subtitle = "Test Subtitle"/>
+  <HomeCard />
+  <JobListings />
 </template>
---
+++ src/components/JobListings.vue
@@ -0,0 +1,17 @@
+<script setup>
+import jobData from '../jobs.json';
+import { ref } from 'vue';
+import JobList from './JobList.vue';
+const jobs = ref(jobData);
+</script>
+
+<template>
+    <section class="bg-blue-50 px-4 py-10">
+        <div class="container-xl lg:container m-auto">
+            <h2 class="text-3xl font-bold text-center text-green-500 mb-6">Latest Jobs</h2>
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <JobList v-for="job in jobs" :key="job.id" :job="job"/>
+            </div>
+        </div>
+    </section>
+</template>
